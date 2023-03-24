16fff2173e1fd591ab9bda2f47445a29bbd84239
phillipedwards <phillip11ace@gmail.com>
2023-03-24 18:32:30 -0400

    Add csv file creation

--- WarehouseSimulation/Program.cs
+++ WarehouseSimulation/Program.cs
@@ -1,12 +1,34 @@
-﻿namespace WarehouseSimulation;
+﻿using System.Text.RegularExpressions;
+
+namespace WarehouseSimulation;
+
+
 class Program
 {
     static void Main(string[] args)
     {
+        prepLogFile();
+
         int numOfDocks = 15;
         (new Warehouse(numOfDocks)).Run();
 
         Console.ReadLine();
+    }
+
+    private static void prepLogFile()
+    {
+        string[] files = Directory.GetFiles("../../../Logs");
+        string pattern = @"\d+";
+
+        int highestValue = 0;
+        foreach (string fileName in files) {
+            string matchValue = Regex.Match(fileName, pattern).Value;
+            int currentValue = int.Parse(matchValue);
+            if (highestValue < currentValue)
+                highestValue = currentValue;
+        }
+
+        File.Create($"../../../Logs/test{highestValue+1}.csv");  
     }
 }
 
